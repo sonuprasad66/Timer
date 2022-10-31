048dcf5db0a99837d28b7f7ddc1b82ce797607ea
sonuprasad66 <sp.srp1999@gmail.com>
2022-10-31 21:28:11 +0530

    Timer_app

---
+++ timer/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Timer from "./Components/Timer";
+
+function App() {
+  return (
+    <div className="App">
+      <Timer />
+    </div>
+  );
+}
+
+export default App;
